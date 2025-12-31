200e96dadf34f76f2a3e12b834b2a4300061a6af
Oskar E. Torres O. <oskar.torres.1234@gmail.com>
2025-12-30 20:17:46 -0500

    [UPDATE]: clear

--- flake.nix
+++ flake.nix
@@ -2,18 +2,11 @@
   description = "Raksodiano's NixOS Configuration";
 
   inputs = {
-    # nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
-    # nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-25.11";
     nur.url = "github:nix-community/NUR";
 
     home-manager.url = "github:nix-community/home-manager";
-
-    # home-manager = {
-    #   url = "github:nix-community/home-manager/release-25.11";
-    #   inputs.nixpkgs.follows = "nixpkgs";
-    # };
 
     noctalia = {
       url = "github:noctalia-dev/noctalia-shell";
