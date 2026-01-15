6542d950efb63ef9d7f2d94e854cba7206f24306
Oskar E. Torres O. <oskar.torres.1234@gmail.com>
2026-01-15 15:46:56 -0500

    [MMM]: I have not idea again

--- home/dotfiles.nix
+++ home/dotfiles.nix
@@ -11,15 +11,9 @@ in
     ".config/bat".source = mkOutOfStoreSymlink "${configDir}/bat";
     ".config/doom".source = mkOutOfStoreSymlink "${configDir}/doom";
     ".config/fastfetch".source = mkOutOfStoreSymlink "${configDir}/fastfetch";
-    ".config/hypr".source = mkOutOfStoreSymlink "${configDir}/hypr";
     ".config/kitty".source = mkOutOfStoreSymlink "${configDir}/kitty";
-    ".config/niri".source = mkOutOfStoreSymlink "${configDir}/niri";
-    ".config/noctalia".source = mkOutOfStoreSymlink "${configDir}/noctalia";
     ".config/rmpc".source = mkOutOfStoreSymlink "${configDir}/rmpc";
     ".config/starship.toml".source = mkOutOfStoreSymlink "${configDir}/starship/starship.toml";
-    ".config/swaync".source = mkOutOfStoreSymlink "${configDir}/swaync";
-    ".config/waybar".source = mkOutOfStoreSymlink "${configDir}/waybar";
-    ".config/wofi".source = mkOutOfStoreSymlink "${configDir}/wofi";
     ".config/tmux".source = mkOutOfStoreSymlink "${configDir}/tmux";
     ".tridactylrc".source = mkOutOfStoreSymlink "${configDir}/tridactyl/tridactylrc";
     ".config/yt-dlp".source = mkOutOfStoreSymlink "${configDir}/yt-dlp";
